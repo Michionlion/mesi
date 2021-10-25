ce66fdcde73e14cd7d81e54817392085c78c974f
Saejin Mahlau-Heinert <saejinmh@va.wagner.com>
2021-10-25 05:08:40 -0400

    Add prepare module

--- mesi/prepare.py
+++ mesi/prepare.py
@@ -1,1 +1,17 @@
 """Prepare user input for comparisons."""
+
+from itertools import combinations
+from pathlib import Path
+from typing import List, Tuple
+
+
+def all_possible_combinations(files: List[Path]) -> List[Tuple[Path, Path]]:
+    """Generate all possible pair combinations of files.
+
+    Args:
+        files (List[Path]): A list of Paths to pair
+
+    Returns:
+        List[Tuple[Path, Path]]: A list of the generated pairs
+    """
+    return list(combinations(files, 2))
