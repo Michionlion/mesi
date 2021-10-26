3383545b7f5a29d8e7d561b7a7bf66ab863b6470
Saejin Mahlau-Heinert <saejinmh@va.wagner.com>
2021-10-25 23:29:45 -0400

    Add tests for display.get_distinct_file_names

--- tests/test_display.py
+++ tests/test_display.py
@@ -1,1 +1,29 @@
 """Test the display module."""
+
+from os import PathLike
+from pathlib import Path
+from typing import List, Tuple
+import pytest
+from mesi import display
+
+
+def path_tuple(one: PathLike, two: PathLike):
+    return (Path(one), Path(two))
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (
+            path_tuple("test-1/hello.txt", "test-2/hello.txt"),
+            ("test-1", "test-2"),
+        ),
+        (path_tuple("test-1/json.txt", "test-1/jqsn.txt"), ("json.txt", "jqsn.txt")),
+        (
+            path_tuple("test-1/json.txt", "test-2/jqsn.txt"),
+            ("test-1/json.txt", "test-2/jqsn.txt"),
+        ),
+    ],
+)
+def test_get_distinct_file_names(input: Tuple[Path, Path], expected: Tuple[str, str]):
+    assert display.get_distinct_file_names(input) == expected
